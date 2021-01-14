26f1936bee16bc646ef8b112369560114a471390
Madoxen <pchlad5@gmail.com>
2021-01-14 11:29:34 +0000

    Map now shows up

--- app/src/main/java/com/example/openlooper/MainActivity.kt
+++ app/src/main/java/com/example/openlooper/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.openlooper
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceManager
+import org.osmdroid.config.Configuration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
     }
 }

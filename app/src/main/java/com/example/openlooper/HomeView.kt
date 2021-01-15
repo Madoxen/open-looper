8ac7c7571e35fe0b0e39615293273e3b6d9a235b
Dariusz <dariusz.momot98@gmail.com>
2021-01-15 21:14:11 +0100

    add clickable menu button with show favorite routes
    Basic view favorite routes

--- app/src/main/java/com/example/openlooper/HomeView.kt
+++ app/src/main/java/com/example/openlooper/HomeView.kt
@@ -1,20 +1,27 @@
 package com.example.openlooper
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.core.view.get
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.preference.PreferenceManager
 import com.example.openlooper.VM.RouteVM
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenu
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.fragment_home_view.*
+import kotlinx.android.synthetic.main.fragment_home_view.view.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
@@ -30,6 +37,9 @@ class HomeView : Fragment() {
     lateinit var mBottomSheet: LinearLayout;
     lateinit var mBottomBehavior: BottomSheetBehavior<View>;
     lateinit var mBottomFAB: FloatingActionButton
+    lateinit var mBottomNavigationView: BottomNavigationView
+    lateinit var mFavoriteSide: NavigationView
+
     var track: Polyline? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +58,12 @@ class HomeView : Fragment() {
         mBottomFAB = view.findViewById(R.id.bottom_FAB)
         mBottomSheet = view.findViewById(R.id.bottom_sheet_swipe)
         mBottomBehavior = BottomSheetBehavior.from(mBottomSheet.findViewById(R.id.bottom_sheet_swipe))
+        mBottomNavigationView = view.findViewById(R.id.bottom_nav_view)
+        mFavoriteSide = view.findViewById((R.id.favorite_side))
+
+        //Remove weird navbar view
+        mBottomAppBar.bottom_nav_view.setBackground(null)
+
         Configuration.getInstance().load(this.context, PreferenceManager.getDefaultSharedPreferences(this.context));
         map = view.findViewById(R.id.mapview);
         //Set some defaults
@@ -65,12 +81,22 @@ class HomeView : Fragment() {
             map.overlayManager.add(track);
         })
 
-
         vm.getRoute();
 
+
+        mBottomBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
         mBottomFAB.setOnClickListener {
             mBottomBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED)
         }
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.favorite_buttom -> view.drawer_layout_home.openDrawer(GravityCompat.START)
+                R.id.record_buttom -> Log.v("Route", "There record your route")
+            }
+            true
+        }
+
         return view
     }
 

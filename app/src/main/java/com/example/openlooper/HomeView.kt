41f2ae63316c140150d03c4325df2b659275c27d
Dariusz <dariusz.momot98@gmail.com>
2021-01-29 16:25:05 +0100

    Fix crash when user add empty route

--- app/src/main/java/com/example/openlooper/HomeView.kt
+++ app/src/main/java/com/example/openlooper/HomeView.kt
@@ -16,6 +16,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -154,7 +155,18 @@ class HomeView : Fragment() {
 
         mAddFavouriteFAB.setOnClickListener() {
             vm.currentRoute.value?.let {
-                vmFav.addFavorite(Favorite(0,"New favourite route!",vm.getRouteTotalLength(), it))
+                if(vm.getRouteTotalLength() != 0.0){
+                    vmFav.addFavorite(
+                        Favorite(
+                            0,
+                            "New favourite route!",
+                            vm.getRouteTotalLength(),
+                            it
+                        )
+                    )
+                }else{
+                    Toast.makeText(this.context, "This route is empty !" , Toast.LENGTH_LONG).show()
+                }
             }
         }
 
